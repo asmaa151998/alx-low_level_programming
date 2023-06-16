f2822109ed7cae0c614957264fc7e56a2ed4b40f
asmaa151998 <sousou.asouma15@gmail.com>
2023-06-16 02:11:19 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Enter point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+	for (x = 'a' ; x <= 'z' ; x++)
+	     putchar (x);
+	for (x = 'A' ; x <= 'Z' ; x++)
+	     putchar (x);
+	putchar ('\n');
+	return (0);
+}
