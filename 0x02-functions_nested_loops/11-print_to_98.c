216f1bbf743d00d1123ead70d2c7d4be7fe30be3
asmaa151998 <sousou.asouma15@gmail.com>
2023-06-20 12:39:21 -0700

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * print_to_98 - print n to 98 counts
- * separated by comma, followed by space 
+ * separated by comma, followed by space
  * number should be printed in order
  * @n: input
 */
@@ -11,11 +11,10 @@
 void print_to_98(int n)
 {
 	int x;
-	
 	if (n > 98)
 		for (x = n; x > 98; x--)
 			printf("%d, ", x);
-	else 
+	else
 		for (x = n; x < 98; x++)
 			printf("%d, ", x);
 	printf("98\n");
