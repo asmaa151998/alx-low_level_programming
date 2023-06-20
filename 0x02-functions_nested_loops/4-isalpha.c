9c902ca0d5c47bb18fc5442714f81c60891bb52e
asmaa151998 <sousou.asouma15@gmail.com>
2023-06-20 12:32:13 -0700

    commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isalpha - Checks for alphabetic character 
+ * _isalpha - Checks for alphabetic character
  * @c: The character to be checked
  * Return: 1 if c is a letter, otherwise 0
  */
