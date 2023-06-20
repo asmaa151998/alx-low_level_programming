258ab5c163633e9cc3241289148c54b5702921aa
asmaa151998 <sousou.asouma15@gmail.com>
2023-06-20 09:47:12 -0700

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,15 @@ int print_last_digit(int x)
 {
 	int y;
 	y = x % 10;
+
 	if (y < 0)
 	{
-		_putchar(-y + 0);
+		_putchar(-y + 48);
 		return (-y);
 	}
 	else
 	{
-		_putchar(y + 0);
+		_putchar(y + 48);
 		return (y);
 	}
 }
