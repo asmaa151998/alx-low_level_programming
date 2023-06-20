dd285aa09ebd5fe3c9fc41d0285975985059b58b
asmaa151998 <sousou.asouma15@gmail.com>
2023-06-20 12:09:38 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
-
 void print_alphabet(void)
 {
 	char alphabet;
+
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		_putchar(alphabet);
 	_putchar('\n');
