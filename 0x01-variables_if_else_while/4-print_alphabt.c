098513cbee22f8b9a03c9c8fc25fb09a046cbaa9
asmaa151998 <sousou.asouma15@gmail.com>
2023-06-25 10:35:03 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Prints the alphabet without q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'e' && x != 'q')
+		{
+			putchar(x);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
