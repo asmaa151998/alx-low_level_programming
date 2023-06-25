ef78de147b2993aa88b9af67396121ec61505ff9
asmaa151998 <sousou.asouma15@gmail.com>
2023-06-25 08:34:06 -0700

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,11 @@
 int main(void)
 {
 	char x;
+
 	for (x = 'a' ; x <= 'z' ; x++)
-	     putchar (x);
-	for (x = 'A' ; x <= 'Z' ; x++)
-	     putchar (x);
+	{
+		putchar (x);
+	}
 	putchar ('\n');
 	return (0);
 }
