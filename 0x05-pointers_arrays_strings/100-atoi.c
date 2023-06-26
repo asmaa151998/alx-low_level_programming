5c71539aee2b698ef84df8d026ce6c1c4396ee5a
asmaa151998 <sousou.asouma15@gmail.com>
2023-06-26 05:38:44 -0700

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -32,7 +32,7 @@ int _atoi(char *s)
 				digit = -digit;
 			a = a * 10 + digit;
 			b = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			if (s[x + 1] < '0' || s[x + 1] > '9')
 				break;
 			b = 0;
 		}
